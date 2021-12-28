cc6a4fb99abf68a4a5371e1be14d0859ec342ab9
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2021-12-28 17:48:29 -0300

    Upadate

--- calcula-troco.js
+++ calcula-troco.js
@@ -14,13 +14,11 @@ const notas = [100, 50, 20, 10, 5, 2, 1, 0.50, 0.25, 0.10, 0.05]
 var notasCliente = []
 
 function calculaTroco(troco){
-    console.log("chamou a função")
     var listaConstruida = false
     var indice = 0
     while(listaConstruida == false){
-        console.log("entrou no while")
         if(notas[indice] <= troco){
-            notasCliente.push(notas[indice])
+            notasCliente.push(notas[indice].toFixed(2))
             troco = (troco - notas[indice]).toFixed(2)
             if(notas[indice] <= troco){
                 listaConstruida = false
@@ -29,9 +27,16 @@ function calculaTroco(troco){
                 listaConstruida = false
             }
         } else {
-            listaConstruida = true
-            console.log(notasCliente)
-            console.log(troco)
+            if(notas[indice] > troco && !(troco < 0.05)){
+                indice += 1
+                listaConstruida = false
+            } else {
+                listaConstruida = true
+                var notasClienteFormatadas = notasCliente.join([separador = ", "])
+                notasCliente = []
+                console.log(troco)
+                return notasClienteFormatadas
+            }
         }
     }
 }
