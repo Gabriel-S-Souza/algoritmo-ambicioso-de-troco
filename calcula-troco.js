0ec599935c0cd765837a8602c5cb3222f08320b5
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2021-12-28 14:56:56 -0300

    updade da lógica

--- calcula-troco.js
+++ calcula-troco.js
@@ -1,40 +1,39 @@
-
 const saida = document.querySelector("#saida")
-var notasTrocoCopy = []
 
- function main(){
+ function principal(){
     const input = document.querySelector("#troco")
     const button = document.querySelector("button")
     button.addEventListener("click", (e)=>{
         var troco = input.value
-        calculaTroco(troco)
-        saida.textContent = notasTrocoCopy
+        var notasTroco = calculaTroco(troco)
+        saida.textContent = notasTroco
     })
 }
 
 const notas = [100, 50, 20, 10, 5, 2, 1, 0.50, 0.25, 0.10, 0.05]
-var notasTroco = []
+var notasCliente = []
 
-function calculaTroco(troco, indice = 0){
-    if(notas[indice] <= troco){
-        console.log([indice, notas.length, indice + 1 < notas.length])
-        notasTroco.push(notas[indice])
-        troco = (troco - notas[indice]).toFixed(2)
-        if(notas[indice] <= troco) calculaTroco(troco, indice)
-        else if(indice + 1 < notas.length) calculaTroco(troco, indice + 1)
-        else {
-            console.log(notasTroco)
-            notasTrocoCopy = notasTroco
-            notasTroco = []
-        } 
-    } else if(indice + 1 < notas.length){
-        console.log([indice, notas.length, indice + 1 < notas.length])
-        calculaTroco(troco, indice + 1)
-    } else {
-        console.log(notasTroco)
-        notasTrocoCopy = [...notasTroco]
-        notasTroco = []
-    } 
+function calculaTroco(troco){
+    console.log("chamou a função")
+    var listaConstruida = false
+    var indice = 0
+    while(listaConstruida == false){
+        console.log("entrou no while")
+        if(notas[indice] <= troco){
+            notasCliente.push(notas[indice])
+            troco = (troco - notas[indice]).toFixed(2)
+            if(notas[indice] <= troco){
+                listaConstruida = false
+            } else {
+                indice += 1
+                listaConstruida = false
+            }
+        } else {
+            listaConstruida = true
+            console.log(notasCliente)
+            console.log(troco)
+        }
+    }
 }
 
-main()
+principal()
